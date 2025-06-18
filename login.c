65806bc40604edd8bcff3f870244b2e56f58ca5f
Emilio-BC <emiliobaas@outlook.com>
2025-06-18 11:35:51 -0600

    Create login.c
    codigo de login para examen de C

---
+++ login.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+#include <string.h>
+
+//vamos a hacer que el estudiante ingrese su matricula y su nombre y que luego la maquina imprima esa info]
+
+int main(){
+    char name[100];
+    char id[100];
+printf("Ingresa tu nombre:");
+// fgets lee hasta el salto de linea o n-1 caracteres 
+// stdin es el flujo de entrada estandar (teclado)
+fgets(name, sizeof(name), stdin);
+name[strcspn(name, "\n")] = 0;
+printf("Ingrese su matricula:");
+fgets(id, sizeof(id), stdin);
+id[strcspn(id, "\n")]=0;
+//esa ultima linea elimina el salto de linea que puede incluir fgets
+
+
+printf("su usuario y matricula son: %s y %s\n", name, id);
+return 0;
